ba0ee142afb4888e9acd874b799af0e8d5e60dcd
Ranis Zyalalov <Jabelya@yandex.ru>
2025-07-28 19:23:25 +0300

    Перенос проекта

---
+++ stats/stats-client/src/main/java/ewm/client/StatsClient.java
@@ -0,0 +1,40 @@
+package ewm.client;
+
+import ewm.CreateEndpointHitDto;
+import ewm.EndpointStatDto;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class StatsClient {
+    private final RestClient restClient = RestClient.builder().baseUrl("http://stats-server:9090").build();
+
+    public ResponseEntity<Void> sendHit(CreateEndpointHitDto createEndpointHitDto) {
+        return restClient.post()
+                .uri("/hit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(createEndpointHitDto)
+                .retrieve()
+                .toEntity(Void.class);
+    }
+
+    public ResponseEntity<List<EndpointStatDto>> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/stats")
+                        .queryParam("start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                        .queryParam("end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                        .queryParam("uris", uris)
+                        .queryParam("unique", unique)
+                        .build())
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<>() {
+                });
+    }
+}
