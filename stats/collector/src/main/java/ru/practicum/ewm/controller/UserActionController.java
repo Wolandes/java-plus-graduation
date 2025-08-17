c3b42b3f43e8452a0a7c71b7aa0fd609a7db8708
Ranis Zyalalov <Jabelya@yandex.ru>
2025-08-17 07:00:12 +0300

    collector сделано

---
+++ stats/collector/src/main/java/ru/practicum/ewm/controller/UserActionController.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewm.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@RequiredArgsConstructor
+@GrpcService
+public class UserActionController {
+}
